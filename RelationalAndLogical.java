eb2f0c9b1631ccaecf6a3d250642f73f37af1767
java <sarulatha4072@gmail.com>
2024-11-15 05:32:00 +0530

    Add files via upload

---
+++ RelationalAndLogical.java
@@ -0,0 +1,28 @@
+package javaprogram;
+
+public class RelationalAndLogical {
+
+	public static void main(String[] args) {
+		
+		int a=12;
+		int b=20;
+		boolean x=true;
+		boolean y=false;
+		System.out.println("Relational operator");
+		System.out.println("a>b is"+" "+(a>b));
+		System.out.println("a<b is"+(a<b));
+		System.out.println("a=>b is"+(a<=b));
+		System.out.println("a<=b is"+(a>=b));
+		System.out.println("a==b is"+(a==b));
+		System.out.println("a!=b is"+(a!=b));
+		
+		System.out.println();
+		
+		System.out.println("logical operrator is ");
+		System.out.println("x && y: " + (x && y)); 
+        System.out.println("x || y: " + (x || y));
+		
+		
+	}
+
+}
