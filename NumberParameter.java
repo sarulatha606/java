a8256d7042bb7302845a97ed58698e7e214375fa
java <sarulatha4072@gmail.com>
2024-11-15 05:32:57 +0530

    Add files via upload

---
+++ NumberParameter.java
@@ -0,0 +1,18 @@
+package javaprogram;
+
+public class NumberParameter {
+	public static int add(int a,int b ) {
+		return(a+b);
+		
+	}
+		
+
+	
+public static void main(String[] args) {
+	int number1=12;
+	int number2=20;
+    int sum = add(number1,number2);
+	System.out.println("the sum is"+sum);
+	
+}
+}
