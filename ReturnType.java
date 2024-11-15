93e3c048648260bdf7fbb8b1d0847b1fc9d8a7a6
java <sarulatha4072@gmail.com>
2024-11-15 05:32:30 +0530

    Add files via upload

---
+++ ReturnType.java
@@ -0,0 +1,18 @@
+package javaprogram;
+
+public class ReturnType {
+	public static int add(int num1,int num2) {
+		return num1+num2;
+	}
+
+	public static void main(String[] args) {
+		
+		int a=18;
+		int c=20;
+		int result=add(a,c);
+		System.out.println("the a value is"+" "+a+"the c value is "+" "+c+" "+"the result is" +" "+ result);
+		
+
+	}
+
+}
