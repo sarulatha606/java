21ad901126b4cb4217ef47539a372210099e5a5c
java <sarulatha4072@gmail.com>
2024-11-19 07:50:09 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,19 @@
+package javaprogram;
+
+public class WhileLoop {
+
+	
+		
+	 public static void main(String[] args) {
+	        int i = 10; 
+	        while (i <= 100) { 
+	            System.out.print(i); 
+	            if (i < 100) { 
+	                System.out.print(", ");             }
+	            i += 10; 
+	        }
+	    }
+
+	}
+
+
