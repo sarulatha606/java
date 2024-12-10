ef0624181a79f315a0dc10d5bac30f5a5f7419c2
java <sarulatha4072@gmail.com>
2024-12-10 23:59:18 +0530

    Add files via upload

---
+++ Heartshapeusingfor.java
@@ -0,0 +1,19 @@
+package java2;
+
+public class Heartshapeusingfor {
+	public static void main(String args[])
+	{
+		for(int i=0;i<6;i++)
+		{
+			for(int j=0;j<7;j++)
+			{
+				if((i==0&& j%3!=0)||(i==1&&j%3==0)||i-j==2||i+j==8)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
